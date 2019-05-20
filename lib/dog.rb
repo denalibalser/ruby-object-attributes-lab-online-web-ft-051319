20c701129006430ec50aa5b21b21dbfecdd5a528
Denali Balser <denali.balser167@gmail.com>
2019-05-20 19:10:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,9 +10,9 @@ class Dog
   end
   def instantialize(breed)
     @breed = breed
-  end 
+  end
   def breed
-    @breed = breed
+    @breed
   end
   def breed= (new_breed)
     @breed = new_breed
