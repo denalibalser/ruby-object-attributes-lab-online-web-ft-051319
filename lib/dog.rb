d0db98584b5044c0ec0babe6742d279f3c2fe764
Denali Balser <denali.balser167@gmail.com>
2019-05-20 19:09:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,10 +8,10 @@ class Dog
   def name= (new_name)
     @name = new_name
   end
-  def breed(breed)
+  def breed
     @breed = breed
   end
   def breed= (new_breed)
     @breed = new_breed
-  end 
+  end
 end
